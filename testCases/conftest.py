55934f5472027393fff65e833746779dfffc9f9b
naazshaikh <naazashaikh@hotmail.com>
2021-08-25 16:51:14 +0200

    updated conftest.py for html reports

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -3,6 +3,38 @@ import pytest
 
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome(executable_path=r"C:\Users\NaazA\PycharmProjects\nopcommerce\Browser\chromedriver.exe")
+def setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome(
+            executable_path=r"C:\Users\NaazA\PycharmProjects\nopcommerce\Browser\chromedriver.exe")
+    elif browser == 'edge':
+        driver = webdriver.Edge(executable_path="C:\\Users\\NaazA\\PycharmProjects\\nopcommerce\\Browser"
+                                                "\\msedgedriver.exe")
+    else:
+        driver = webdriver.Chrome(
+            executable_path=r"C:\Users\NaazA\PycharmProjects\nopcommerce\Browser\chromedriver.exe")
+
     return driver
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+##############pytest HTML Reports ##############
+
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'nop commerce'
+    config._metadata['Module Name'] = 'Customers'
+    config._metadata['tester'] = 'Naaz'
+
+
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
